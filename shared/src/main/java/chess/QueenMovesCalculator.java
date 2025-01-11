175ed9115daa6f0d545b1145b26efd7f55c17db5
maxamilllion823 <max_merrell@hotmail.com>
2025-01-11 14:03:09 -0700

    Passed Queen Move Tests

---
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -0,0 +1,15 @@
+package chess;
+
+import java.util.Collection;
+
+public class QueenMovesCalculator extends PieceMovesCalculator {
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        // the eight directions a queen can move
+        int[][] directions = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+        return pieceMovesHelper(board, myPosition, directions, false);
+    }
+
+}
