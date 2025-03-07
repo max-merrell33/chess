260b489933eed9cc2ad9ea17aac1ffb4b1fedcbe
maxamilllion823 <max_merrell@hotmail.com>
2025-03-06 20:05:45 -0700

    Implemented SQLUserDAO

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -2,16 +2,113 @@ package dataaccess;
 
 import model.UserData;
 
-public class SQLUserDAO implements UserDAO {
-    public void createUser(UserData userData) throws DataAccessException {
+import java.sql.SQLException;
+import java.sql.ResultSet;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class SQLUserDAO implements UserDAO {
+
+    public SQLUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    public void createUser(UserData userData) throws DataAccessException {
+        var statement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        executeUpdate(statement, userData.username(), userData.password(), userData.email());
     }
 
     public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM pet WHERE username=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, username);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readUser(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
     public void deleteAllUsers() throws DataAccessException {
+        var statement = "TRUNCATE pet";
+        executeUpdate(statement);
+    }
 
+    private UserData readUser(ResultSet rs) throws SQLException {
+        var username = rs.getString("username");
+        var password = rs.getString("password");
+        var email = rs.getString("email");
+        return new UserData(username, password, email);
+    }
+
+    private void executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    switch (param) {
+                        case String p -> ps.setString(i + 1, p);
+                        case Integer p -> ps.setInt(i + 1, p);
+                        case null -> ps.setNull(i + 1, NULL);
+                        default -> {}
+                    }
+                }
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                `username` VARCHAR(255) NOT NULL,
+                `password` VARCHAR(255) NOT NULL,
+                `email` VARCHAR(255) NOT NULL,
+                PRIMARY KEY (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+                `authToken` VARCHAR(255) NOT NULL,
+                `username` VARCHAR(255) NOT NULL,
+                PRIMARY KEY (`authToken`),
+                FOREIGN KEY (`username`) REFERENCES users(`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS games (
+                `gameID` INT NOT NULL AUTO_INCREMENT,
+                `whiteUsername` VARCHAR(255) NOT NULL,
+                `blackUsername` VARCHAR(255) NOT NULL,
+                `gameName` VARCHAR(255) NOT NULL,
+                `game` TEXT DEFAULT NULL,
+                PRIMARY KEY (`gameID`),
+                FOREIGN KEY (`whiteUsername`) REFERENCES users(`username`),
+                FOREIGN KEY (`blackUsername`) REFERENCES users(`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
     }
 }
